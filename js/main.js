fb10da9bd879e5980f25d760a9330798f8017bb4
manojpramesh <manojpramesh@gmail.com>
2016-09-23 15:55:39 +0530

    Style modifications

--- js/main.js
+++ js/main.js
@@ -3,5 +3,13 @@
 
 	$(".header-img").height($(window).height());
 	
+	new WOW().init();
+
+	var topMenu = $("#top-menu");			   
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > topMenu.position().top) topMenu.addClass("stick");
+		else topMenu.removeClass("stick");
+	});
+
 })($);
 
