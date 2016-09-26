292e37639bc5a87a4a768aedbfc82ef34e2e5228
manojpramesh <manojpramesh@gmail.com>
2016-09-26 17:03:56 +0530

    portfolio and contact

--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
-(function($) {
+(function ($) {
 	'use strict';
 
 	$(".header-img").height($(window).height());
-	
+	$(window).resize(function () {
+		$(".header-img").height($(window).height());
+	});
+
 	new WOW().init();
 
-	var topMenu = $("#top-menu");			   
-	$(window).scroll(function() {
+	var topMenu = $("#top-menu");
+	$(window).scroll(function () {
 		if ($(window).scrollTop() > topMenu.position().top) topMenu.addClass("stick");
 		else topMenu.removeClass("stick");
+	});
+
+	$(".main-menu ul.navbar-nav li ").on('click', function () {
+		$(".main-menu ul.navbar-nav li").removeClass("active");
+		$(this).addClass("active");
+	});
+
+
+	$('a.page-scroll').bind('click', function (event) {
+		var _this = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(_this.attr('href')).offset().top - 55
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
+
+	$('#portfolio-content').mixItUp({
+		animation: {
+			animation: {
+				duration: 400,
+				effects: 'fade translateX(10%) stagger(34ms) translateY(10%) scale(0.01)',
+				easing: 'cubic-bezier(0.25, 0.46, 0.45, 0.94)'
+			}
+		}
 	});
 
 })($);
