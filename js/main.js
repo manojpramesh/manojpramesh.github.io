01f8c3cd5115c1ed80c92501cdd8a999afb1e4ba
manojpramesh <manojpramesh@gmail.com>
2016-09-29 20:04:49 +0530

    Hobbies

--- js/main.js
+++ js/main.js
@@ -38,5 +38,16 @@
 		}
 	});
 
+	var owl = $("#owl-demo"); 
+	owl.owlCarousel({
+		items : 5, //5 items above 1000px browser width
+		itemsDesktop : [1000,4], //5 items between 1000px and 901px
+		itemsDesktopSmall : [900,3], // betweem 900px and 601px
+		itemsTablet: [600,1], //2 items between 600 and 0
+		itemsMobile : false,
+		autoPlay: true,
+		pagination: false
+	});	
+
 })($);
 
